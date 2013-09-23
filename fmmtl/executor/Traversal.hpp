bc77db4e9efd0057a565204cc3e1cb838370ebf2
Cris Cecka <ccecka@seas.harvard.edu>
2013-09-23 19:38:21 -0400

    Traversal concept updates

--- fmmtl/executor/Traversal.hpp
+++ fmmtl/executor/Traversal.hpp
@@ -1,7 +1,7 @@
 #pragma once
 /** @file Traversal
- * @brief A simple queue-based tree traversal that passes box pairs to an
- * @a Evaluators near_field and far_field operators.
+ * @brief A simple queue-based tree traversal that passes box pairs to
+ * near and far field evaluators.
  */
 
 /**
@@ -13,22 +13,23 @@
  *   box_iterator child_end() const;
  * }
  *
- * concept Evaluator {
- *   void near_field(source_box&, target_box&);
- *   // @returns whether box pair will be handled as a far field
- *   bool far_field(source_box&, target_box&);
+ * concept NearEvaluator {
+ *   // Handles a box pair with near field evaluation
+ *   void operator()(source_box&, target_box&);
+ * }
+ * concept FarEvaluator {
+ *   // @returns whether box pair will be handled as a far field or not
+ *   bool operator()(source_box&, target_box&);
  * }
  */
 
+#include <utility>
 #include <queue>
 
 template <typename SourceBox, typename TargetBox>
 struct Traversal {
   typedef SourceBox source_box;
   typedef TargetBox target_box;
-
-  typedef typename source_box::box_iterator source_box_iterator;
-  typedef typename target_box::box_iterator target_box_iterator;
 
   typedef std::pair<source_box, target_box> box_pair;
 
@@ -78,8 +79,8 @@ struct Traversal {
   template <class FarEvaluator>
   inline static void split_source(source_box& sbox, target_box& tbox,
                                   FarEvaluator& far_eval, box_queue& pairQ) {
-    source_box_iterator c_end = sbox.child_end();
-    for (source_box_iterator cit = sbox.child_begin(); cit != c_end; ++cit) {
+    auto c_end = sbox.child_end();
+    for (auto cit = sbox.child_begin(); cit != c_end; ++cit) {
       source_box cbox = *cit;
       interact(cbox, tbox, far_eval, pairQ);
     }
@@ -88,8 +89,8 @@ struct Traversal {
   template <class FarEvaluator>
   inline static void split_target(source_box& sbox, target_box& tbox,
                                   FarEvaluator& far_eval, box_queue& pairQ) {
-    target_box_iterator c_end = tbox.child_end();
-    for (target_box_iterator cit = tbox.child_begin(); cit != c_end; ++cit) {
+    auto c_end = tbox.child_end();
+    for (auto cit = tbox.child_begin(); cit != c_end; ++cit) {
       target_box cbox = *cit;
       interact(sbox, cbox, far_eval, pairQ);
     }
