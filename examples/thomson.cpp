6f7bea39933989a68be5d2bcf6417ec535b9cea5
Cris Cecka <ccecka@seas.harvard.edu>
2014-08-21 20:07:36 -0400

    Thomson corrections and touchups

--- examples/thomson.cpp
+++ examples/thomson.cpp
@@ -16,8 +16,8 @@
 
 int main(int argc, char **argv)
 {
-  std::size_t N = 60000;
-  bool checkErrors = true;
+  // Number of points
+  std::size_t N = 10000;
 
   // Parse custom command line args
   for (int i = 1; i < argc; ++i) {
@@ -30,8 +30,9 @@ int main(int argc, char **argv)
   FMMOptions opts = get_options(argc, argv);
 
   // Init kernel
+  const unsigned p = 7;   // Order of the approximation
   typedef LaplaceSpherical kernel_type;
-  kernel_type K;
+  kernel_type K(p);
 
   typedef kernel_type::point_type  point_type;  // Vec<3,double>
   typedef kernel_type::charge_type charge_type; // double
@@ -46,7 +47,8 @@ int main(int argc, char **argv)
   for (auto&& p : points) {
     double mag;
     do {
-      p = fmmtl::random<point_type>::get();
+      p = fmmtl::random<point_type>::get();  // point in [ 0,1]^DIM
+      p = 2*p - 1;                           // point in [-1,1]^DIM
       mag = norm_2(p);
     } while (mag > 1);
     p /= mag;
@@ -57,9 +59,8 @@ int main(int argc, char **argv)
 
   std::cout << "Atoms Generated" << std::endl;
 
-  long double E = 0;
   long double lastE = 0;
-  double scale = 100;
+  double scale = 1;
 
   for (unsigned iter = 1; iter < 300; ++iter) {
     // Build the FMM with the current points
@@ -69,27 +70,44 @@ int main(int argc, char **argv)
     // Compute forces and energy
     std::vector<result_type> pot_force = A * charges;
 
-    // Compute total energy
-    E = 0;
-    for (auto&& r : pot_force)
-      E += r[0];
 
-    // If the decrease is small, scale down
-    // TODO: better scheduling/scaling for step size
-    if (lastE - E <= 0)
-      scale /= 2;
+    // Compute the maximum cross product as a gradient magnitude proxy
+    // and the total energy
+    double max_cross_sq = 0;
+    long double E = 0;
+    for (std::size_t k = 0; k < N; ++k) {
+      // Get the force part of the result
+      const result_type& pf = pot_force[k];
+      E += pf[0];
+
+      point_type f = point_type(pf[1], pf[2], pf[3]);
+      max_cross_sq = std::max(max_cross_sq, norm_2_sq(cross(points[k],f)));
+    }
+    // Full matvec double counts potential interactions
+    E /= 2;
+
+    // Step size heuristic for the gradient descent
+    // TODO: Armijo/Wolfe/Raleigh
+    if (lastE <= E)
+      scale *= 0.5;
+    else
+      scale *= 1.1;
     lastE = E;
-    scale *= 1.1;
+
+    // Use the scale and gradient magnitude proxy to compute the step size
+    if (max_cross_sq == 0) break;
+    double step = scale / (N * std::sqrt(max_cross_sq));
 
     // Print iteration data
     std::cout << "Iter: " << iter << "\n";
-    std::cout << "  Scale: " << scale << "\n";
-    std::cout << "  Energy: " << std::setprecision(10) << E << std::endl;
+    std::cout << "  Grad:   " << std::setprecision(4) << max_cross_sq << "\n";
+    std::cout << "  Scale:  " << std::setprecision(4) << scale << "\n";
+    std::cout << "  Energy: " << std::setprecision(20) << E << std::endl;
 
     // Update from forces and normalize
     for (std::size_t k = 0; k < N; ++k) {
-      for (std::size_t d = 0; d < points[k].size(); ++d)
-        points[k][d] += scale * pot_force[k][1+d];
+      for (std::size_t d = 0; d < 3; ++d)
+        points[k][d] -= step * pot_force[k][1+d];
       points[k] /= norm_2(points[k]);
     }
   }
